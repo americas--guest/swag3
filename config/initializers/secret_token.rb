82b736f0a32ab26011e54d5c0bd24f7fec0ba986
Anisul <anisul.huq90@gmail.com>
2013-12-25 18:19:36 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Swag3::Application.config.secret_key_base = '4e956efab0c286910b8cc482dd74b62a82b111c589efdd9ac6c3dd6d213d1702f4706b9e529dcb7d53a67f85255ad717c9f564fd852407a4ffa2e02fb276195b'
