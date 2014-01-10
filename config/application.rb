3c3524d39d6c7ac578312361dcd7f705f6b2b13c
Anisul <anisul.huq90@gmail.com>
2014-01-10 15:23:10 -0500

    remove asset initializer

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,8 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+
+
 module Swag3
   class Application < Rails::Application
     
@@ -32,6 +34,5 @@ module Swag3
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
-    config.assets.initialize_on_precompile = false
   end
 end
